6c877891343a01e6f5397fcdeeb59dc24c199d5e
asniayu <asniayu30@gmail.com>
2025-05-16 17:11:03 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
 st.title("hello bubies")
-st.write(
-    "Afghan berubah jadi harimau malaya"
-)
-
+st.write("Afghan berubah jadi harimau malaya")
 st.image("1746503376035.jpg", width=200) 
 st.write("\n")  
 st.subheader("yaya")
